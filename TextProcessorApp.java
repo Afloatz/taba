d4e639180c16d0ea1820f0736d995bf89b615def
Afloatz <florentlaot.fr@gmail.com>
2021-05-21 14:33:28 +0100

    question 1.b

--- TextProcessorApp.java
+++ TextProcessorApp.java
@@ -13,24 +13,36 @@ public class TextProcessorApp {
     public static void main(String[] args) {
 
         // declare local variables
-		Scanner in = new Scanner(System.in);
 		String myText;
+
+		Scanner input = new Scanner(System.in);
+
+        // input
+		// prompt the user for the number of paragraphs
+		System.out.println("enter the number of paragraghs you want to encode:");
+		int n = input.nextInt();
 
 		TextProcessor encoder = new TextProcessor();
 
-		// input
-		// prompt the user to provide a paragraph
-		System.out.println("enter a paragragh: ");
-		myText = in.nextLine();
-		encoder.setText(myText);
+		// next, we use a loop statement because the users can enter as many paragraph as they want, namely the number of paragraphs stored above in the n variable
+		for (int i = 0; i < n; i++){	
+			// prompt the user to provide a paragraph
+			System.out.println("enter a paragragh: ");
+			input = new Scanner(System.in);
+			myText = input.nextLine();
+			// use the setter method to store the user's text into the object's instance variable text
+			encoder.setText(myText);
 
-		// processing
-		encoder.encodeText();
+			// processing: call the method encodeText() to apply the ER2 rule to encode the text
+			encoder.encodeText();
 
-		// output
-		String myEncodedText = encoder.getEncodedText();
-		System.out.println("Encoded paragraph is: " + myEncodedText);
-        
+			// output
+			// use the getter method to retrieve the encoded text
+			String myEncodedText = encoder.getEncodedText();
+			System.out.println("Encoded paragraph is: " + myEncodedText);
+			System.out.println("==================================================================================================================================");
+
+		}  
     }
     
 }
